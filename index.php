ea936614a867b3fe3115d0439d6eefe152b63be0
zSupre <pacinivitor@gmail.com>
2020-08-07 12:23:52 -0300

    Página inicial incompleta

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<title>Bem vindo a nossa loja</title>
+	<meta charset="utf-8">
+	<link href="css/estilo.css" rel="stylesheet">
+</head>
+<body>
+
+<?php
+  $nome = "Paulo";
+?>
+
+<nav>
+	<ul>
+		<li>Cadastre-se</li>
+		<li>Área restrita</li>
+	</ul>
+</nav>
+  
+   Categorias
+   produtos
+
+</body>
+</html>
